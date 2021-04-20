fa62d15ff461d20400a1befc0ca628ad80b4fcd2
Paola Fuentes Caro <paolafc95@gmail.com>
2021-04-20 18:09:45 -0500

    deploy.sh added

--- tallerapp/vue.config.js
+++ tallerapp/vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-  publicPath: '/Taller-Framework-JS/'
+  module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/Taller-Framework-JS/'
+      : '/'
   }
